dbf3c05291173825e4029a330eb78d1a05874a41
M.K <miika.kallasoja@gmail.com>
2023-01-14 21:03:44 +0200

    Class refactor

--- app.js
+++ app.js
@@ -6,92 +6,103 @@ import createRecognizer from './speech-recognition.js'
 
 dotenv.config()
 
-// Constants
-const DIRECTORY = process.env.RECORDINGS_DIRECTORY
-const KEEP_RECORDINGS = process.env.KEEP_RECORDINGS == 'true'
 
 
-if (!fs.existsSync(DIRECTORY)) {
-    fs.mkdirSync(DIRECTORY);
-}
+class Main {
+    constructor(audioRecorder, createRecognizer) {
+    this.DIRECTORY = process.env.RECORDINGS_DIRECTORY
+    this.LANGUAGE = process.env.RECOGNITION_LANGUAGE
+    this.recorder = audioRecorder;
+    this.createRecognizer = createRecognizer;
+    console.log(this.createRecognizer)
+    this.filename = '';
 
-const fileName = path.join(DIRECTORY, 'speech.wav');
+    // Log information on the following events.
+    this.recorder.on('error', function () {
+        console.warn('Recording error.');
+    });
 
-
-main()
-
-function main() {
-// Create path to write recordings to.
-
-
-
-// Create file path with random name.
-// const fileName = path.join(
-//     DIRECTORY,
-//     Math.random()
-//     .toString(36)
-//     .replace(/[^0-9a-zA-Z]+/g, '')
-//     .concat('.wav')
-// );
-
-// Create write stream.
-const fileStream = fs.createWriteStream(fileName, {
-    encoding: 'binary'
-});
-
-
-// Start and write to the file.
-audioRecorder.start().stream().pipe(fileStream);
-console.log('Now recording...')
-// Keep process alive.
-process.stdin.resume();
-console.warn('Press ctrl+c to exit.');
-
-// Log information on the following events.
-audioRecorder.on('error', function () {
-    console.warn('Recording error.');
-});
-
-audioRecorder.on('close', recognize);
+    this.recorder.on('close', this.recognize.bind(this));
 
 }
-
-
-
-
-function recognize() {
-    const language = process.env.RECOGNITION_LANGUAGE;
-
-    let recognizer = createRecognizer(fileName, language);
-    recognizer.recognizeOnceAsync(
-        result => {
-             if (!result.privJson) {
-                console.error(`There was an error connecting to the speech recognition service. Details: ${result.privErrorDetails}`)
-             } else {
-            const resultJson = JSON.parse(result.privJson)
-            if (resultJson.DisplayText) {
-                console.log(`You said: ${resultJson.DisplayText}`)
-            } else {
-                console.log('Sorry, what you said was not recognizable')
-            }
-            recognizer.close();
-            recognizer = undefined;
-            fs.unlinkSync(fileName)
-            
-
+    createDirectory() {
+        if (!fs.existsSync(this.DIRECTORY)) {
+            fs.mkdirSync(this.DIRECTORY);
         }
     }
-        ,
-        err => {
-            console.trace("err - " + err);
+    createFile() {
+        // Create file path with random name.
+        const fileName = path.join(
+            this.DIRECTORY,
+            Math.random()
+            .toString(36)
+            .replace(/[^0-9a-zA-Z]+/g, '')
+            .concat('.wav')
+        );
 
-            recognizer.close();
-            recognizer = undefined;
-            fs.unlinkSync(fileName)
+         this.fileName = fileName;
+    }
+
+    recordToFile() {
+        // Create write stream.
+        const fileStream = fs.createWriteStream(this.fileName, {
+            encoding: 'binary'
+        });
+
+        // Start and write to the file.
+        this.recorder.start().stream().pipe(fileStream);
+        console.log('Now recording...')
+        // Keep process alive.
+        process.stdin.resume();
+        console.warn('Press ctrl+c to exit.');
+    }
+
+    recognize() {
+        let recognizer = this.createRecognizer(this.fileName, this.LANGUAGE);
+        recognizer.recognizeOnceAsync(
+            result => {
+                if (!result.privJson) {
+                    console.error(`There was an error connecting to the speech recognition service. Details: ${result.privErrorDetails}`)
+                } else {
+                    const resultJson = JSON.parse(result.privJson)
+                    if (resultJson.DisplayText) {
+                        console.log(`You said: ${resultJson.DisplayText}`)
+                    } else {
+                        console.log('Sorry, what you said was not recognizable')
+                    }
+                    recognizer.close();
+                    recognizer = undefined;
+                    fs.unlinkSync(this.fileName)
+
+                }
+            },
+            err => {
+                console.trace("err - " + err);
+
+                recognizer.close();
+                recognizer = undefined;
+                fs.unlinkSync(this.fileName)
+
+
             }
 
-        }
-        
         );
-        
+
+    }
+
+
 }
+
+const app = new Main(audioRecorder, createRecognizer)
+
+app.createDirectory();
+app.createFile();
+app.recordToFile();
+
+
+
+
+
+
+
+
