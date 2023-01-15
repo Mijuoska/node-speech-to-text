6332039a1d0ca21d242773798847ae7a3eb0b011
M.K <miika.kallasoja@gmail.com>
2023-01-15 13:00:24 +0200

    uncomment audiorecorder settings

--- recorder.js
+++ recorder.js
@@ -11,14 +11,14 @@ const DEBUG_MODE = process.env.DEBUG_MODE == 'true'
 // If an option is not given the default value, as seen below, will be used.
 const options = {
 program: `sox`, // Which program to use, either `arecord`, `rec`, or `sox`.
-  //  device: null, // Recording device to use, e.g. `hw:1,0`
+    device: null, // Recording device to use, e.g. `hw:1,0`
     bits: 16, // Sample size. (only for `rec` and `sox`)
     channels: 1, // Channel count.
     encoding: `signed-integer`, // Encoding type. (only for `rec` and `sox`)
     rate: 16000, // Sample rate.
     type: `wav`, // Format type.
 
-//    silence: 1, // Duration of silence in seconds before it stops recording.
+    silence: 1, // Duration of silence in seconds before it stops recording.
     thresholdStart: 0.5, // Silence threshold to start recording.
     thresholdStop: 0.5, // Silence threshold to stop recording.
     keepSilence: false, // Keep the silence in the recording.
